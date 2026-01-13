807fd567eda331658511465eac1d0328b29b520a
srinu <nenavath2013@gmail.com>
2026-01-13 06:55:28 +0530

    first commit

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "eks-prod-dev-tf-state"
     key            = "terraform/dev.tfstate"  # path inside the bucket
     region         = "ap-south-1"
-    dynamodb_table = "eks-prod-dev-tf-lock"
+    use_lockfile = "eks-prod-dev-tf-lock"
     encrypt        = true
   }
 }
