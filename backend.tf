3457426912a12d7885ff6b3470bd58b56b5f6d00
Nenavath srinu Naik <45934081+nenavathsrinu@users.noreply.github.com>
2026-01-14 04:46:45 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-incedo-autmoation-lock"
-    key            = "terraform/dev.tfstate"  # path inside the bucket
-    region         = "ap-south-1"
-    use_lockfile   = true
-    encrypt        = true
+    encrypt      = true
+    use_lockfile = true
   }
 }
